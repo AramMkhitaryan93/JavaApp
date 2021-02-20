9fb62a8020bcdd045b82af136477834420aecbbe
AramMkhitaryan93 <artsvaberd21@gmail.com>
2021-02-20 17:05:08 +0400

    adding DeleteMains class

--- src/SchoolManagementSystem/School.java
+++ src/SchoolManagementSystem/School.java
@@ -1,5 +1,14 @@
 package SchoolManagementSystem;
 
+/**
+ * Many teacher,many students.
+ * Implements teacher and students
+ * using an ArrayList
+ * Created by Aram on 20/02/2021.
+ */
 public class School {
-
+    private Teacher teacher;
+    private Students students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
 }
