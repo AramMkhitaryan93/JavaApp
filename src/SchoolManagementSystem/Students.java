4c717febdc2ae4229ab93d0cde19d3428d3cd6bd
AramMkhitaryan93 <artsvaberd21@gmail.com>
2021-02-20 14:53:31 +0400

    Java application school management system

---
+++ src/SchoolManagementSystem/Students.java
@@ -0,0 +1,13 @@
+package SchoolManagementSystem;
+
+public class Students {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Students(int id,String name,int grade){
+        
+    }
+}
