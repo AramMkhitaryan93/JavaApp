99abb138f17f94efc5e652a1785f364c9f5534ac
AramMkhitaryan93 <artsvaberd21@gmail.com>
2021-02-22 15:13:34 +0400

    creating  the account class

--- src/atmBankSystem/Account.java
+++ src/atmBankSystem/Account.java
@@ -1,4 +1,26 @@
 package atmBankSystem;
 
+import java.util.ArrayList;
+
 public class Account {
+    /**
+     * The name of the account.
+     */
+    private String name;
+    /**
+     * The balance of the account.
+     */
+    private double balance;
+    /**
+     * The account ID number.
+     */
+    private String uuid;
+    /**
+     * The user object that owns this account.
+     */
+    private User holder;
+    /**
+     * The list of transaction for this user.
+     */
+    private ArrayList<Transaction> transactions;
 }
