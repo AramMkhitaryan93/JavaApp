37ed8115c270c588ce8563659d92a75a46d9c185
AramMkhitaryan93 <artsvaberd21@gmail.com>
2021-02-22 19:08:49 +0400

    Create a new user of the bank

--- src/atmBankSystem/Bank.java
+++ src/atmBankSystem/Bank.java
@@ -77,4 +77,46 @@ public class Bank {
         this.accounts.add(anAcct);
     }
 
+    /**
+     * Create a new user of the bank
+     *
+     * @param firstName the user's first name
+     * @param lastName  the user's last name
+     * @param pin       the user's pin
+     * @return the new User object
+     */
+    public User addUsr(String firstName, String lastName, String pin) {
+        // create a new User object and add iy to list
+        User newUser = new User(firstName, lastName, pin, this);
+        this.users.add(newUser);
+        //crate a savings account for the user
+        //accounts lists
+        Account newAccount = new Account("Savings", newUser, this);
+        newUser.addAccount(newAccount);
+        this.accounts.add(newAccount);
+        return newUser;
+    }
+
+    /**
+     * Get the User object associated with a particular userId and  pin if they
+     * are valid
+     *
+     * @param userID the UUID of the user to log in
+     * @param pin    the pin of the user
+     * @return the User object ,if the login is successful ,or null if
+     * it is not
+     */
+    public User userLogin(String userID, String pin) {
+        //search through list of users
+
+        for (User u : this.users) {
+
+            // check user ID is correct
+            if (u.getUUID().compareTo(userID) == 0 && u.validatePin(pin)) {
+                return u;
+            }
+        }
+        //if we haven't found the user or have an incorrect pin
+        return null;
+    }
 }
